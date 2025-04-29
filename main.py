fd086355bc72f7557018bb14d8bd7aff407d0a07
stelldyyst <70965706+stelldyyst@users.noreply.github.com>
2025-04-29 17:36:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ app = FastAPI()
 model = load_model("best_model_new.keras")
 label_binarizer = joblib.load("label_binarizer.pkl")
 
+@app.get("/")
+def read_root():
+    return {"message": "Сервис работает. Отправьте POST-запрос на /predict/"}
+
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     try:
@@ -18,17 +22,16 @@ async def predict(file: UploadFile = File(...)):
         image = preprocess_image(image_bytes)
 
         # Предсказание
-        probs = model.predict(image)[0]  # Вероятности
+        probs = model.predict(image)[0]
         predicted_index = np.argmax(probs)
         predicted_label = label_binarizer.classes_[predicted_index]
 
         return JSONResponse({
             "predicted_class": str(predicted_label),
             "probabilities": {
-                str(label): float(prob) 
+                str(label): float(prob)
                 for label, prob in zip(label_binarizer.classes_, probs)
             }
         })
-
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
